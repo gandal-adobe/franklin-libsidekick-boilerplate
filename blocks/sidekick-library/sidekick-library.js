62a4baded3bebe78f3f7dfeb12da638c9ec10034
Dylan Depass <dylandepass@gmail.com>
2023-04-19 10:42:28 -0400

    fix: update script location and custom element name

--- blocks/sidekick-library/sidekick-library.js
+++ blocks/sidekick-library/sidekick-library.js
@@ -1,9 +1,9 @@
-import 'https://main--franklin-library-host--dylandepass.hlx.live/library/library.js';
+import 'https://main--franklin-library-host--dylandepass.hlx.live/tools/sidekick/library/index.js';
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   const blockConfig = readBlockConfig(block);
-  const library = document.createElement('franklin-library');
+  const library = document.createElement('sidekick-library');
   library.config = {};
 
   Object.keys(blockConfig).forEach((attr) => {
